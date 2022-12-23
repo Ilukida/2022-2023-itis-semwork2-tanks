2e10464d0840cf589019d99933a08c9a52ee1aba
Alina Vaskina <vaskina.alina.ru@mail.ru>
2022-12-23 04:54:52 +0300

    add semwork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+
+    public Main(Gameplay gameplay) {
+        setTitle("Tanks");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(10, 10, 800, 630);
+        setResizable(false);
+        setBackground(Color.black);
+        add(gameplay);
+        setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Gameplay gamePlay = new Gameplay();
+        Main main = new Main(gamePlay);
     }
 }
